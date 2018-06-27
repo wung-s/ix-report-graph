d25f54103c982c742438cd987555eabc8ccaaa7f
Wung S <wungmathing.shaiza@mac-1319-ws.local>
2018-06-27 16:56:56 -0400

    :new: Add header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,16 @@
-import React from "react"
+import React, { Fragment } from "react"
 import Chart from "./Chart"
 
 const Home = props => {
-  return <Chart reportsBySiteName={props.reportsBySiteName} />
+  return (
+    <Fragment>
+      <h1 style={{ textAlign: "center", color: "grey" }}>
+        {" "}
+        IX Hackathon 2018{" "}
+      </h1>
+      <Chart reportsBySiteName={props.reportsBySiteName} />
+    </Fragment>
+  )
 }
 
 export default Home
