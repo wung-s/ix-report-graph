a4faaf97c6e372e74bf5905d901f9c9e595fd610
Wung S <wungmathing.shaiza@mac-1319-ws.local>
2018-06-27 11:46:18 -0400

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react"
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import HomeContainer from "./containers/HomeContainer"
 
 class App extends Component {
   render() {
-    return <HomeContainer />
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={HomeContainer} />
+          <Route path="/about" component={HomeContainer} />
+        </div>
+      </Router>
+    )
   }
 }
 
